fc79c9a524e64bb1cf8ab7e49a2998bd035d64a3
Tatyana Alexander <tatyana.theresa@gmail.com>
2021-06-14 14:38:58 -0400

    Added bind context using .bind()

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,20 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import "./Counter.css";
 
 export default class Counter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 10
-    }
+      count: 10,
+      name: "Tatyana",
+    };
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleIncrement() {
+    console.log("hello world");
+    // update the count variable
+    this.setState((currentState) => {
+      console.log(currentState);
+    });
   }
 
   render() {
     return (
       <div className="Counter-container">
-        Counter
+        <h1>{this.state.name}'s Counter App</h1>
         <h2>{this.state.count}</h2>
+        <button onClick={this.handleIncrement}>increment (+)</button>
       </div>
-    )
+    );
   }
 }
